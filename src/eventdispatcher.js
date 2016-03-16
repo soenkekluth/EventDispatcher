a6dcd116d575919c76cf3690b94d897792aa00db
Sönke Kluth <soenke.kluth@gmail.com>
2016-03-16 20:43:19 +0100

    optimize to standard eventdisptatcher implementation && add helper methods

--- src/eventdispatcher.js
+++ src/eventdispatcher.js
@@ -21,132 +21,114 @@
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  **/
 
-(function(exports) {
+ 'use strict';
 
-    'use strict';
+ function isEmpty(obj) {
+   for (var prop in obj) {
+     if (obj.hasOwnProperty(prop)){
+       return false;
+     }
+   }
+   return true;
+ }
+
+ var EventDispatcher = function() {
+   this._eventMap = {};
+   this._destroyed = false;
+ };
+
+ EventDispatcher.prototype.addListener = function(event, listener) {
+   var firstListener = false;
+   var listener = this.getListener(event);
+   if (listener === null) {
+     firstListener = true;
+     listener = this._eventMap[event] = [];
+   }
+   if (firstListener || listener.indexOf(listener) === -1) {
+     listener.push(listener);
+     return true;
+   }
+   return false;
+ };
+
+ EventDispatcher.prototype.addListenerOnce = function(event, listener) {
+   var s = this;
+   var f2 = function() {
+     s.removeListener(event, f2);
+     return listener.apply(this, arguments);
+   };
+   return this.addListener(event, f2);
+ };
+
+ EventDispatcher.prototype.removeListener = function(event, listener) {
+
+   var listeners = this.getListener(event);
+   if (listeners) {
+     var i = listeners.indexOf(listener);
+     if (i > -1) {
+       this._eventMap[event] = listeners.splice(i, 1);
+       if (listeners.length === 0) {
+         delete(this._eventMap[event]);
+       }
+       return true;
+     }
+   }
+   return false;
+ };
+
+ EventDispatcher.prototype.removeAllListener = function(event) {
+   var listeners = this.getListener(event);
+   if (listeners) {
+     this._eventMap[event].length = 0;
+     delete(this._eventMap[event]);
+   }
+   return true;
+ };
+
+ EventDispatcher.prototype.hasListener = function(event) {
+   return this.getListener(event) !== null;
+ };
+
+ EventDispatcher.prototype.hasListeners = function() {
+   return this._eventMap && !isEmpty(this._eventMap);
+ };
+
+ EventDispatcher.prototype.dispatch = function(eventType, eventObject) {
+   var listeners = this.getListener(eventType);
+
+   if (listeners) {
+     eventObject = (eventObject) ? eventObject : {};
+     eventObject.type = eventType;
+     eventObject.target = eventObject.target || this;
+     var i = -1;
+     while (++i < listeners.length) {
+       listeners[i].call(null, eventObject);
+     }
+     return true;
+   }
+   return false;
+ };
+
+ EventDispatcher.prototype.getListener = function(event) {
+   var result = this._eventMap ? this._eventMap[event] : null;
+   return (result || null);
+ };
+
+ EventDispatcher.prototype.destroy = function() {
+   if (this._eventMap) {
+     for (var i in this._eventMap) {
+       this.removeAllListener(i);
+     }
+     this._eventMap = null;
+   }
+   this._destroyed = true;
+ };
 
 
-    //IE8
-    if (!Array.prototype.indexOf) {
-        Array.prototype.indexOf = function(obj, start) {
-            for (var i = (start || 0), j = this.length; i < j; i++) {
-                if (this[i] === obj) {
-                    return i;
-                }
-            }
-            return -1;
-        }
-    }
+ //Method Map
+ EventDispatcher.prototype.on = EventDispatcher.prototype.bind = EventDispatcher.prototype.addEventListener = EventDispatcher.prototype.addListener;
+ EventDispatcher.prototype.off = EventDispatcher.prototype.unbind = EventDispatcher.prototype.removeEventListener = EventDispatcher.prototype.removeListener;
+ EventDispatcher.prototype.once = EventDispatcher.prototype.one = EventDispatcher.prototype.addListenerOnce;
+ EventDispatcher.prototype.trigger = EventDispatcher.prototype.dispatchEvent = EventDispatcher.prototype.dispatch;
 
-    var EventDispatcher = function() {
-
-    };
-
-    EventDispatcher.prototype = {
-
-        _eventMap: {},
-        _destroyed: false,
-
-        addListener: function(event, listener) {
-
-            this.getListener(event) || (this._eventMap[event] = []);
-
-            if (this.getListener(event).indexOf(listener) == -1) {
-                this._eventMap[event].push(listener);
-            }
-
-            return this;
-        },
-
-
-        addListenerOnce: function(event, listener) {
-            var s = this;
-            var f2 = function() {
-                s.removeListener(event, f2);
-                return listener.apply(this, arguments);
-            };
-            return this.addListener(event, f2);
-        },
-
-        removeListener: function(event, listener) {
-
-            var listeners = this.getListener(event);
-            if (listeners) {
-                var i = listeners.indexOf(listener);
-                if (i > -1) {
-                    this._eventMap[event] = listeners.splice(i, 1);
-                    if (listeners.length === 0) {
-                        delete(this._eventMap[event]);
-                    }
-                }
-            }
-
-            return this;
-        },
-
-
-        removeAllListener: function(event) {
-
-            var listeners = this.getListener(event);
-            if (listeners) {
-                this._eventMap[event].length = 0;
-                delete(this._eventMap[event]);
-            }
-            return this;
-        },
-
-
-
-        dispatch: function(event, eventObject) {
-
-            var listeners = this.getListener(event);
-
-            if (listeners) {
-
-                //var args = Array.prototype.slice.call(arguments, 1);
-                eventObject = (eventObject) ? eventObject : {};
-                eventObject.type = event;
-                eventObject.target = eventObject.target || this;
-                var i = -1;
-                while (++i < listeners.length) {
-
-                    //args ? listeners[i].apply(null, args) : listeners[i]();
-                    listeners[i].call(null, eventObject);
-                }
-            } else {
-                // console.info('Nobody is listening to ' + event);
-            }
-
-            return this;
-        },
-
-        getListener: function(event) {
-            if (this._destroyed) {
-                throw new Error('I am destroyed');
-            }
-            return this._eventMap[event];
-        },
-
-        destroy: function() {
-            if (this._eventMap) {
-                for (var i in this._eventMap) {
-                    this.removeAllListener(i);
-                }
-                //TODO leave an empty object is better then throwing an error when using a fn after destroy?
-                this._eventMap = null;
-            }
-            this._destroyed = true;
-        }
-    };
-
-    //Method Map
-    EventDispatcher.prototype.on = EventDispatcher.prototype.bind = EventDispatcher.prototype.addEventListener = EventDispatcher.prototype.addListener;
-    EventDispatcher.prototype.off = EventDispatcher.prototype.unbind = EventDispatcher.prototype.removeEventListener = EventDispatcher.prototype.removeListener;
-    EventDispatcher.prototype.once = EventDispatcher.prototype.one = EventDispatcher.prototype.addListenerOnce;
-    EventDispatcher.prototype.trigger = EventDispatcher.prototype.dispatchEvent = EventDispatcher.prototype.dispatch;
-
-    (typeof module != "undefined" && module.exports) ? (module.exports = EventDispatcher) : (typeof define != "undefined" ? (define(function() {
-        return EventDispatcher;
-    })) : (exports.EventDispatcher = EventDispatcher));
-
-})(this);
+ module.exports = EventDispatcher;

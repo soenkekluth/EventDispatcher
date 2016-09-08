aab5a05b8bb9af89f8c1017dba64e1096b2248c2
Sönke Kluth <mail@soenkekluth.com>
2016-09-08 14:25:18 +0200

    babel rewrite

---
+++ dist/index.js
@@ -0,0 +1,1 @@
+'use strict';var EventDispatcher=require('./eventdispatcher');module.exports=EventDispatcher;
---
+++ src/eventdispatcher.js
@@ -0,0 +1,124 @@
+ 'use strict';
+
+ function isEmpty(obj) {
+   for (var prop in obj) {
+     if (obj.hasOwnProperty(prop)) {
+       return false;
+     }
+   }
+   return true;
+ }
+
+ var _instanceMap = {};
+
+ export default class EventDispatcher {
+
+
+   constructor() {
+     this._eventMap = {};
+     this._destroyed = false;
+
+     //Method Map
+     this.on = this.bind = this.addEventListener = this.addListener;
+     this.off = this.unbind = this.removeEventListener = this.removeListener;
+     this.once = this.one = this.addListenerOnce;
+     this.emmit = this.trigger = this.dispatchEvent = this.dispatch;
+
+   }
+
+   static getInstance(key) {
+     if (!key) {
+       throw new Error('key must be');
+     }
+     return _instanceMap[key] || (_instanceMap[key] = new EventDispatcher());
+   }
+
+
+   addListener(event, listener) {
+     var listeners = this.getListener(event);
+     if (!listeners) {
+       this._eventMap[event] = [listener];
+     } else if (listeners.indexOf(listener) === -1) {
+       listeners.push(listener);
+     }
+     return this;
+   }
+
+   addListenerOnce(event, listener) {
+     var s = this;
+     var f2 = () => {
+       s.removeListener(event, f2);
+       return listener.apply(this, arguments);
+     };
+     return this.addListener(event, f2);
+   }
+
+   removeListener(event, listener) {
+
+     if (typeof listener === 'undefined') {
+       return this.removeAllListener(event);
+     }
+
+     var listeners = this.getListener(event);
+     if (listeners) {
+       var i = listeners.indexOf(listener);
+       if (i > -1) {
+         listeners = listeners.splice(i, 1);
+         if (!listeners.length) {
+           delete(this._eventMap[event]);
+         }
+       }
+     }
+     return this;
+   }
+
+   removeAllListener(event) {
+     var listeners = this.getListener(event);
+     if (listeners) {
+       this._eventMap[event].length = 0;
+       delete(this._eventMap[event]);
+     }
+     return this;
+   }
+
+   hasListener(event) {
+     return this.getListener(event) !== null;
+   }
+
+   hasListeners() {
+     return (this._eventMap !== null && this._eventMap !== undefined && !isEmpty(this._eventMap));
+   }
+
+   dispatch(eventType, eventObject) {
+     var listeners = this.getListener(eventType);
+
+     if (listeners) {
+       eventObject = eventObject || {};
+       eventObject.type = eventType;
+       eventObject.target = eventObject.target || this;
+
+       var i = -1;
+       while (++i < listeners.length) {
+         listeners[i](eventObject);
+       }
+       // return true;
+     }
+     return this;
+   }
+
+   getListener(event) {
+     var result = this._eventMap ? this._eventMap[event] : null;
+     return (result || null);
+   }
+
+   destroy() {
+     if (this._eventMap) {
+       for (var i in this._eventMap) {
+         this.removeAllListener(i);
+       }
+       this._eventMap = null;
+     }
+     this._destroyed = true;
+     return this;
+   }
+ }
